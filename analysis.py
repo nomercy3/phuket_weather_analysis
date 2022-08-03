331fcd7c150242be5405ef38cb5c5caeec82ea0e
Vasily <wasjaus@gmail.com>
2022-08-03 14:18:33 +0700

    analysis finished

--- analysis.py
+++ analysis.py
@@ -1,19 +1,29 @@
 import pandas as pd
-from cleaning import preparation
 
 
 def precipitation_score_month(dataframe: pd.DataFrame):
-  return dataframe.groupby(["Date Month"])["Score For Month"].sum().reset_index(name="Precipitation Score")
+  return dataframe.groupby(['Date Month'])['Score For Month'].sum().reset_index(name='Precipitation Score')
 
 
 def mean_score_month(dataframe: pd.DataFrame):
-  return dataframe.groupby(["Date Month"])["Score For Month"].mean().reset_index()
+  return dataframe.groupby(['Date Month'])['Score For Month'].mean().reset_index()
 
 
 def calc_most_rainy_day(dataframe: pd.DataFrame, aggregated: pd.DataFrame):
-  max_values: pd.DataFrame = aggregated.groupby(["Date Month"])["Max In Month"].max().reset_index(name="Most Rainy Day (Value)")
-  # max_values.rename(columns={"Max In Month": "Most Rainy Day (Value)"}, inplace=True)
+  max_values_df: pd.DataFrame = aggregated.groupby(['Date Month'])['Max In Month'].max().reset_index(name='Most Rainy Day (Value)')
 
-  # print(max_values_dates)
+  dates = list()
+  for month, value in zip(
+      max_values_df['Date Month'].values,
+      max_values_df['Most Rainy Day (Value)'].values
+  ):
 
-  # print(max_values)
+    dates.append(dataframe.loc[
+      (dataframe['Weather Condition Score'] == value) &
+      (dataframe['Date Month'] == month),
+      'Date (String)'
+    ].head(1).item())
+
+  max_values_df['Most Rainy Day (Date)'] = dates
+
+  return max_values_df
--- main.py
+++ main.py
@@ -1,31 +1,37 @@
-from cleaning import preparation, make_weather_code_table, calculate_score, grouped_score_by_month, max_score_value_by_month
+from cleaning import prepare_data, make_weather_code_df, calculate_score, grouped_score_by_month, max_score_value_by_month
 from analysis import precipitation_score_month, mean_score_month, calc_most_rainy_day
 import pandas as pd
 
 
 data = pd.read_csv('/Users/vasilypopov/developer/jupyter_notebooks/phuket_weather_analysis/Phuket, Weather, Daily 2015-2021.csv')
 
+# Analysis main flow
+
 
 def main():
     # Clean data
-    daily_df = preparation(data)
+    daily_df = prepare_data(data)
 
     # Calculate score for each row
-    daily_df["Weather Condition Score"] = daily_df.apply(lambda row: calculate_score(daily_df, row, make_weather_code_table(daily_df)), axis=1)
+    daily_df['Weather Condition Score'] = daily_df.apply(lambda row: calculate_score(daily_df, row, make_weather_code_df(daily_df)), axis=1)
 
-    # Created separate df for aggregated values. Helping us later with analysis
+    # Created separate df for aggregated values. It will help us later with the analysis
     aggregated_df = pd.DataFrame(grouped_score_by_month(daily_df))
     aggregated_df.reset_index(inplace=True)
-    aggregated_df.rename(columns={"Weather Condition Score": "Score For Month"}, inplace=True)
+    aggregated_df.rename(columns={'Weather Condition Score': 'Score For Month'}, inplace=True)
 
     # Created max in month
-    aggregated_df["Max In Month"] = max_score_value_by_month(daily_df)["Weather Condition Score"]
+    aggregated_df['Max In Month'] = max_score_value_by_month(daily_df)['Weather Condition Score']
 
-    analysis_df = precipitation_score_month(aggregated_df)
-    analysis_df["Mean Precipitation Score"] = mean_score_month(aggregated_df)["Score For Month"]
-    print(aggregated_df)
-    print(analysis_df)
-    calc_most_rainy_day(daily_df, aggregated_df)
+    precipitation_score_df = precipitation_score_month(aggregated_df)
+    precipitation_score_df['Mean Precipitation Score'] = mean_score_month(aggregated_df)['Score For Month']
+
+    # inner join
+    analysis_df = pd.merge(
+        precipitation_score_df,
+        calc_most_rainy_day(daily_df, aggregated_df),
+        on=['Date Month']
+    )
 
 
 if __name__ == '__main__':
